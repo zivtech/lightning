37fc3484eafd02ee1c68407e77dab3b057af732b
Phéna Proxima <djphenaproxima@gmail.com>
2016-01-22 09:22:50 -0500

    Added more images to the Hero Imageblock, for carousel purposes.

--- modules/lightning_features/lightning_layout/src/Plugin/Block/Hero.php
+++ modules/lightning_features/lightning_layout/src/Plugin/Block/Hero.php
@@ -7,16 +7,27 @@ use Drupal\Core\Block\BlockBase;
 /**
  * @Block(
  *   id = "hero",
- *   admin_label = @Translation("Hero Image")
+ *   admin_label = @Translation("Hero Carousel")
  * )
  */
 class Hero extends BlockBase {
 
   public function build() {
     return [
+      '#attached' => [
+        'library' => [
+          'lightning_layout/slick',
+        ],
+      ],
       '#markup' => <<<END
 <div>
   <img class="img-responsive" src="/profiles/lightning/modules/lightning_features/lightning_layout/drone1.jpg" alt="Drone racing, TNG" />
+</div>
+<div>
+  <img class="img-responsive" src="/profiles/lightning/modules/lightning_features/lightning_layout/operator.jpg" alt="Drone operator" />
+</div>
+<div>
+  <img class="img-responsive" src="/profiles/lightning/modules/lightning_features/lightning_layout/drone2.jpg" alt="The Predator. It will hide in the bushes and spring its awesomeness upon you." />
 </div>
 <h1>The next generation of drone racing</h1>
 END
